b7595681c6a41430b9dae3e9cefc39f834bd8d02
JpedroA <joaopedropaulino@souunisuam.com.br>
2024-10-03 12:26:06 -0300

    tela de detalhes do card

---
+++ lib/view/card_tatail_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:login_page/model/card_detail.dart';
+
+class CardTatailPage extends StatelessWidget {
+  final CardDetail cardDetail;
+  const CardTatailPage({super.key, required this.cardDetail});
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: cardDetail.id,
+      child: SafeArea(
+        child: Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(Icons.close)),
+                Row(
+                  children: [
+                    Image.network(
+                      cardDetail.url,
+                      height: 20,
+                    ),
+                    Text(
+                      cardDetail.title,
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Expanded(
+                  child: Text(
+                    cardDetail.text,
+                    textAlign: TextAlign.justify,
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
